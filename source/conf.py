b4d5f382bc6678f8208204e04419031f747dfda4
jain-shantanu <jainshantanu6@gmail.com>
2025-08-25 16:52:00 +0300

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -26,6 +26,7 @@ exclude_patterns = []
 
 html_theme = 'basic'
 html_static_path = ['_static']
+html_title = 'Documentation for DIRECD'
 
 html_logo = '../logo/DIRECD_logo_rs.jpg'
 html_theme_options = {
